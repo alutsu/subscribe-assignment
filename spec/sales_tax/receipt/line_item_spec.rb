0df0bb2a0a2b62952794b799de5fad90892679c9
João Victor <jovictor.miguel@gmail.com>
2026-01-19 18:19:05 -0300

    test: ensure Receipt calculates totals correctly

---
+++ spec/sales_tax/receipt/line_item_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+RSpec.describe SalesTax::Receipt::LineItem do
+  subject(:line_item) do
+    described_class.new(
+      quantity: 2,
+      name: 'book',
+      total_price: 24.98,
+      tax_amount: 0.00
+    )
+  end
+
+  describe 'attributes' do
+    it 'stores quantity' do
+      expect(line_item.quantity).to eq(2)
+    end
+
+    it 'stores name' do
+      expect(line_item.name).to eq('book')
+    end
+
+    it 'stores total_price' do
+      expect(line_item.total_price).to eq(24.98)
+    end
+
+    it 'stores tax_amount' do
+      expect(line_item.tax_amount).to eq(0.00)
+    end
+  end
+
+  describe 'immutability' do
+    it_behaves_like 'an immutable object'
+    it_behaves_like 'has frozen string attribute', :name
+  end
+end
---
+++ spec/sales_tax/receipt/receipt_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+RSpec.describe SalesTax::Receipt::Receipt do
+  subject(:receipt) { described_class.new(line_items: line_items) }
+
+  let(:line_items) do
+    [
+      SalesTax::Receipt::LineItem.new(quantity: 1, name: 'item1', total_price: 11.00, tax_amount: 1.00),
+      SalesTax::Receipt::LineItem.new(quantity: 1, name: 'item2', total_price: 22.00, tax_amount: 2.00)
+    ]
+  end
+
+  describe '#total_tax' do
+    it 'sums tax amounts from all line items' do
+      expect(receipt.total_tax).to eq(3.00)
+    end
+
+    context 'when no taxes applied' do
+      let(:line_items) do
+        [SalesTax::Receipt::LineItem.new(quantity: 1, name: 'book', total_price: 12.49, tax_amount: 0.00)]
+      end
+
+      it 'returns 0' do
+        expect(receipt.total_tax).to eq(0.00)
+      end
+    end
+  end
+
+  describe '#total' do
+    it 'sums total prices from all line items' do
+      expect(receipt.total).to eq(33.00)
+    end
+  end
+
+  describe 'immutability' do
+    it_behaves_like 'an immutable object'
+
+    it 'has frozen line_items' do
+      expect(receipt.line_items).to be_frozen
+    end
+  end
+end
